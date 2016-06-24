468aae0e0274947e582d20209a59fa48629e32a0
Dazdranagorn <dazdranagorn@rambler.ru>
2016-06-24 13:45:25 +0300

    get condition #4

--- accuweather.php
+++ accuweather.php
@@ -8,12 +8,30 @@ $location = array(
 );
 
 function cGet($message) {
-  if($curl = curl_init()){
-    curl_setopt($curl, CURLOPT_URL, $message);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($curl, CURLOPT_ENCODING , "gzip");
-    $out = curl_exec($curl);
-    curl_close($curl);
+  $out = '';
+  if($handle = curl_init()){
+    curl_setopt($handle, CURLOPT_URL, $message);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($handle, CURLOPT_ENCODING , "gzip");
+    $response = curl_exec($handle);
+    if ($response === false) {
+      $errno = curl_errno($handle);
+      $error = curl_error($handle);
+      error_log("Curl returned error $errno: $error\n");
+      curl_close($handle);
+    }else{
+      $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+      curl_close($handle);
+      $response = json_decode($response, true);
+      if ($http_code != 200) {
+        error_log("Request has failed with error {$response['Code']}: {$response['Message']}\n");
+      //}else{
+        if (isset($response['Message'])) {
+          error_log("Request was successfull: {$response['Message']}\n");
+        }
+        $out = $response;
+      }
+    }
   }
   return $out;
 }
@@ -21,12 +39,12 @@ function cGet($message) {
 $getAddr = cGet('http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='.$accukey.
 	'&q='.$location['tomchak'].'&language=ru-ru&details=true&toplevel=false');
 
-$answer = $getAddr['Key'];
+//$answer = $getAddr['Key'];
 
-/*
+
 $answer = cGet('http://dataservice.accuweather.com/currentconditions/v1/'.$getAddr['Key'].'?apikey='.$accukey.
   '&language=ru-ru&details=false');
-*/
+
 
 
 /*
